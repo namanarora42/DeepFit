20b167ebf38ea4d2cdfc7741d8df38f22d7c0cce
AbhinaavSingh <abhinaavsingh1@gmail.com>
2022-06-24 00:14:02 -0400

    Update deepfit_integration.py

--- deepfit_integration.py
+++ deepfit_integration.py
@@ -103,21 +103,31 @@ def set_video_feed_variables():
     direction = 0
     form = 0
     feedback = "Bad Form."
-    frame_queue = deque(maxlen=150)
-    clf = DeepFitClassifier('models/deepfit_classifier_v3.tflite')
+    frame_queue = deque(maxlen=250)
+    clf = DeepFitClassifier('deepfit_classifier_v3.tflite')
     return cap,count,direction,form,feedback,frame_queue,clf
 
 
-def set_percentage_bar_and_text(elbow_angle):
-    pushup_success_percentage = np.interp(elbow_angle, (90, 160), (0, 100))
-    pushup_progress_bar = np.interp(elbow_angle, (90, 160), (380, 30))
-    return pushup_success_percentage,pushup_progress_bar
+def set_percentage_bar_and_text(elbow_angle, knee_angle, workout_name_after_smoothening):
+    if workout_name_after_smoothening == "pushups":    
+        pushup_success_percentage = np.interp(elbow_angle, (90, 160), (0, 100))
+        pushup_progress_bar = np.interp(elbow_angle, (90, 160), (380, 30))
+        return pushup_success_percentage,pushup_progress_bar
+    # Else only handles squats right now
+    else:
+        pushup_success_percentage = np.interp(knee_angle, (90, 160), (0, 100))
+        pushup_progress_bar = np.interp(knee_angle, (90, 160), (380, 30))
+        return pushup_success_percentage,pushup_progress_bar
 
 def set_body_angles_from_keypoints(get_angle, img, landmark_list):
     elbow_angle = get_angle(img, landmark_list, 11, 13, 15)
     shoulder_angle = get_angle(img, landmark_list, 13, 11, 23)
     hip_angle = get_angle(img, landmark_list, 11, 23,25)
-    return elbow_angle,shoulder_angle,hip_angle
+    elbow_angle_right = get_angle(img, landmark_list, 12, 14, 16)
+    shoulder_angle_right = get_angle(img, landmark_list, 14, 12, 24)
+    hip_angle_right = get_angle(img, landmark_list, 12, 24,26)
+    knee_angle = get_angle(img, landmark_list, 24,26, 28)
+    return elbow_angle,shoulder_angle,hip_angle,elbow_angle_right,shoulder_angle_right,hip_angle_right,knee_angle
 
 def set_smoothened_workout_name(lm_dict, convert_mediapipe_keypoints_for_model, frame_queue, clf, landmark_list):
     inp_pushup = convert_mediapipe_keypoints_for_model(lm_dict, landmark_list)
@@ -126,11 +136,11 @@ def set_smoothened_workout_name(lm_dict, convert_mediapipe_keypoints_for_model, 
     workout_name_after_smoothening = max(set(frame_queue), key=frame_queue.count)
     return "Workout Name: " + workout_name_after_smoothening
 
-def run_full_workout_motion(count, direction, form, elbow_angle, shoulder_angle, hip_angle, pushup_success_percentage, feedback, workout_name_after_smoothening):
-    if workout_name_after_smoothening == "pushups":
+def run_full_workout_motion(count, direction, form, elbow_angle, shoulder_angle, hip_angle, elbow_angle_right, shoulder_angle_right, hip_angle_right, knee_angle, pushup_success_percentage, feedback, workout_name_after_smoothening):
+    if workout_name_after_smoothening.strip() == "pushups":
         if form == 1:
             if pushup_success_percentage == 0:
-                if elbow_angle <= 90 and hip_angle > 160:
+                if elbow_angle <= 90 and hip_angle > 160 and elbow_angle_right <= 90 and hip_angle_right > 160:
                     feedback = "Feedback: Go Up"
                     if direction == 0:
                         count += 0.5
@@ -139,7 +149,7 @@ def run_full_workout_motion(count, direction, form, elbow_angle, shoulder_angle,
                     feedback = "Feedback: Bad Form."
                         
             if pushup_success_percentage == 100:
-                if elbow_angle > 160 and shoulder_angle > 40 and hip_angle > 160:
+                if elbow_angle > 160 and shoulder_angle > 40 and hip_angle > 160 and elbow_angle_right > 160 and shoulder_angle_right > 40 and hip_angle_right > 160:
                     feedback = "Feedback: Go Down"
                     if direction == 1:
                         count += 0.5
@@ -148,26 +158,27 @@ def run_full_workout_motion(count, direction, form, elbow_angle, shoulder_angle,
                     feedback = "Feedback: Bad Form."
         return [feedback, count]
     # For now, else condition handles just squats
-    else:
+    elif workout_name_after_smoothening.strip() == "squats":
         if form == 1:
             if pushup_success_percentage == 0:
-                if elbow_angle <= 90 and hip_angle > 160:
+                if knee_angle < 90:
                     feedback = "Go Up"
                     if direction == 0:
                         count += 0.5
                         direction = 1
                 else:
-                    feedback = "Bad Form."
-                    
+                    feedback = "Feedback: Bad Form."                    
             if pushup_success_percentage == 100:
-                if elbow_angle > 160 and shoulder_angle > 40 and hip_angle > 160:
-                    feedback = "Go Down"
+                if knee_angle > 169:
+                    feedback = "Feedback: Go Down"
                     if direction == 1:
                         count += 0.5
                         direction = 0
                 else:
-                    feedback = "Bad Form."
+                    feedback = "Feedback: Bad Form."
             return [feedback, count]
+    else:
+        return ["Feedback:",0]
 
 def draw_percentage_progress_bar(form, img, pushup_success_percentage, pushup_progress_bar):
     xd, yd, wd, hd = 10, 175, 50, 200
@@ -185,20 +196,20 @@ def display_rep_count(count, img):
 def show_workout_feedback(feedback, img):    
     xf, yf = 85, 70
     cv2.putText(img, feedback, (xf, yf), cv2.FONT_HERSHEY_PLAIN, 2,
-                    (255, 255, 255), 2)
+                    (0,0,0), 2)
 
 def show_workout_name_from_model(img, workout_name_after_smoothening):
     xw, yw = 85, 40
     cv2.putText(img, workout_name_after_smoothening, (xw,yw), cv2.FONT_HERSHEY_PLAIN, 2,
-                    (255, 255, 255), 2)
+                    (0,0,0), 2)
 
-def check_form(elbow_angle, shoulder_angle, hip_angle, form, workout_name_after_smoothening):
+def check_form(elbow_angle, shoulder_angle, hip_angle, elbow_angle_right, shoulder_angle_right, hip_angle_right, knee_angle, form, workout_name_after_smoothening):
     if workout_name_after_smoothening == "pushups":
-        if elbow_angle > 160 and shoulder_angle > 40 and hip_angle > 160:
+        if elbow_angle > 160 and shoulder_angle > 40 and hip_angle > 160 and elbow_angle_right > 160 and shoulder_angle_right > 40 and hip_angle_right > 160:
             form = 1
     # For now, else impleements squats condition        
     else:
-        if elbow_angle > 160 and shoulder_angle > 40 and hip_angle > 160:
+        if knee_angle > 160:
             form = 1
     return form
 
@@ -249,33 +260,72 @@ def main():
         
         #If landmarks exist, get the relevant workout body angles and run workout. The points used are identifiers for specific joints
         if len(landmark_list) != 0:
-            elbow_angle, shoulder_angle, hip_angle = set_body_angles_from_keypoints(get_angle, img, landmark_list)
+            elbow_angle, shoulder_angle, hip_angle, elbow_angle_right, shoulder_angle_right, hip_angle_right, knee_angle = set_body_angles_from_keypoints(get_angle, img, landmark_list)
             
-            pushup_success_percentage, pushup_progress_bar = set_percentage_bar_and_text(elbow_angle)
-        
             workout_name_after_smoothening = set_smoothened_workout_name(lm_dict, convert_mediapipe_keypoints_for_model, frame_queue, clf, landmark_list)    
+
+            workout_name_after_smoothening = workout_name_after_smoothening.replace("Workout Name:", "").strip()
+            pushup_success_percentage, pushup_progress_bar = set_percentage_bar_and_text(elbow_angle, knee_angle, workout_name_after_smoothening)
+        
                     
             #Is the form correct at the start?
-            form = check_form(elbow_angle, shoulder_angle, hip_angle, form, workout_name_after_smoothening)
+            form = check_form(elbow_angle, shoulder_angle, hip_angle, elbow_angle_right, shoulder_angle_right, hip_angle_right, knee_angle, form, workout_name_after_smoothening)
         
             #Full workout motion
-            if (run_full_workout_motion(count, direction, form, elbow_angle, shoulder_angle, hip_angle, pushup_success_percentage, feedback, workout_name_after_smoothening)!= None):
-                feedback, count = run_full_workout_motion(count, direction, form, elbow_angle, shoulder_angle, hip_angle, pushup_success_percentage, feedback, workout_name_after_smoothening)
+            if workout_name_after_smoothening.strip() == "pushups":
+                if form == 1:
+                    if pushup_success_percentage == 0:
+                        if elbow_angle <= 90 and hip_angle > 160 and elbow_angle_right <= 90 and hip_angle_right > 160:
+                            feedback = "Feedback: Go Up"
+                            if direction == 0:
+                                count += 0.5
+                                direction = 1
+                        else:
+                            feedback = "Feedback: Bad Form."
+                                
+                    if pushup_success_percentage == 100:
+                        if elbow_angle > 160 and shoulder_angle > 40 and hip_angle > 160 and elbow_angle_right > 160 and shoulder_angle_right > 40 and hip_angle_right > 160:
+                            feedback = "Feedback: Go Down"
+                            if direction == 1:
+                                count += 0.5
+                                direction = 0
+                        else:
+                            feedback = "Feedback: Bad Form."
+            # For now, else condition handles just squats
+            elif workout_name_after_smoothening.strip() == "squats":
+                if form == 1:
+                    if pushup_success_percentage == 0:
+                        if knee_angle < 90:
+                            feedback = "Go Up"
+                            if direction == 0:
+                                count += 0.5
+                                direction = 1
+                        else:
+                            feedback = "Feedback: Bad Form."                    
+                    if pushup_success_percentage == 100:
+                        if knee_angle > 169:
+                            feedback = "Feedback: Go Down"
+                            if direction == 1:
+                                count += 0.5
+                                direction = 0
+                        else:
+                            feedback = "Feedback: Bad Form."
             
             
             
+     
             
             #Display workout stats        
             display_workout_stats(count, form, feedback, draw_percentage_progress_bar, display_rep_count, show_workout_feedback, show_workout_name_from_model, img, pushup_success_percentage, pushup_progress_bar, workout_name_after_smoothening)
             
             
-            # Transparent Overlay
-            overlay = img.copy()
-            x, y, w, h = 75, 10, 500, 150
-            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 0), -1)      
-            alpha = 0.4  # Transparency factor.
-            # Following line overlays transparent rectangle over the image
-            image_new = cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0)          
+        # Transparent Overlay
+        overlay = img.copy()
+        x, y, w, h = 75, 10, 500, 150
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255,255,255), -1)      
+        alpha = 0.8  # Transparency factor.
+        # Following line overlays transparent rectangle over the image
+        image_new = cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0)          
             
         cv2.imshow('DEEPFIT Workout Trainer', image_new)
         if cv2.waitKey(10) & 0xFF == ord('q'):
